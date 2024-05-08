7c3c1d198ff1672e75d54a1cdb26fe5b60371235
tejas <tejastg007@gmail.com>
2024-05-08 22:14:53 +0530

    forms in vue

---
+++ forms/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<the-form></the-form>
+</template>
+
+<script>
+import TheForm from "./components/TheForm.vue";
+
+export default {
+	name: "App",
+	components: {
+		TheForm,
+	},
+};
+</script>
+
+<style>
+* {
+	box-sizing: border-box;
+}
+
+html {
+	font-family: sans-serif;
+}
+
+body {
+	margin: 0;
+	background-color: #292929;
+}
+</style>
