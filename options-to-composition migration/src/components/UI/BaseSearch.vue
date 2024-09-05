ff5d2b62c9423056494751171526d0f664370864
tejas <tejastg007@gmail.com>
2024-09-05 11:24:54 +0530

    options to composition migration

---
+++ options-to-composition migration/src/components/UI/BaseSearch.vue
@@ -0,0 +1,35 @@
+<template>
+  <form>
+    <input
+      type="search"
+      @input="search"
+      :value="searchTerm"
+      placeholder="Filter items"
+    />
+  </form>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+const emit = defineEmits(['search']);
+defineProps(['searchTerm']);
+function search(e) {
+  emit('search', e.target.value );
+}
+</script>
+
+<style scoped>
+input {
+  font: inherit;
+  width: 100%;
+  display: block;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus {
+  outline: none;
+  border-color: #00006b;
+  background-color: #eeeeff;
+}
+</style>
---
+++ options-to-composition migration/src/components/projects/ProjectsList.vue
@@ -0,0 +1,111 @@
+<template>
+  <base-container v-if="user">
+    <h2>{{ user.fullName }}: Projects</h2>
+    <base-search
+      @search="updateSearch"
+      :search-term="enteredSearchTerm"
+    ></base-search>
+    <ul v-if="hasProjects">
+      <project-item
+        v-for="prj in availableProjects"
+        :key="prj.id"
+        :title="prj.title"
+      ></project-item>
+    </ul>
+    <h3 v-else>No projects found.</h3>
+  </base-container>
+  <base-container v-else>
+    <h3>No user selected.</h3>
+  </base-container>
+</template>
+
+<script setup>
+import ProjectItem from './ProjectItem.vue';
+import { ref, toRef, computed, defineProps, watch } from 'vue';
+
+let enteredSearchTerm = ref('');
+let activeSearchTerm = ref('');
+
+const props = defineProps(['user']);
+let userRef = toRef(props, 'user');
+const hasProjects = computed(() => {
+  return props.user.projects && availableProjects.value.length > 0;
+});
+
+const availableProjects = computed(() => {
+  if (activeSearchTerm.value) {
+    return props.user.projects.filter((project) => {
+      return project.title.includes(activeSearchTerm.value);
+    });
+  }
+  return props.user.projects;
+});
+
+function updateSearch(val) {
+  enteredSearchTerm.value = val;
+}
+
+watch(enteredSearchTerm, (newVal) => {
+  setTimeout(() => {
+    if (newVal === enteredSearchTerm.value) {
+      activeSearchTerm.value = newVal;
+    }
+  }, 300);
+});
+
+watch(userRef, () => {
+  console.log('user changed');
+  enteredSearchTerm.value = '';
+});
+
+// export default {
+//   components: {
+//     ProjectItem,
+//   },
+//   props: ['user'],
+//   data() {
+//     return {
+//       enteredSearchTerm: '',
+//       activeSearchTerm: '',
+//     };
+//   },
+//   computed: {
+//     hasProjects() {
+//       return this.user.projects && this.availableProjects.length > 0;
+//     },
+//     availableProjects() {
+//       if (this.activeSearchTerm) {
+//         return this.user.projects.filter((prj) =>
+//           prj.title.includes(this.activeSearchTerm)
+//         );
+//       }
+//       return this.user.projects;
+//     },
+//   },
+//   methods: {
+//     updateSearch(val) {
+//       this.enteredSearchTerm = val;
+//     },
+//   },
+//   watch: {
+//     enteredSearchTerm(val) {
+//       setTimeout(() => {
+//         if (val === this.enteredSearchTerm) {
+//           this.activeSearchTerm = val;
+//         }
+//       }, 300);
+//     },
+//     user() {
+//       this.enteredSearchTerm = '';
+//     },
+//   },
+// };
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
